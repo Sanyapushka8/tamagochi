59ccb10af640250a83c17ff238770110ee9a955c
Sanyapushka8 <zimin.sasha.2020@mail.ru>
2025-03-07 13:54:49 +0300

    Update octopus.py

--- octopus.py
+++ octopus.py
@@ -10,37 +10,98 @@ class Octopus(Tamagochi):
          '/|||\`
            '|`   
        """
+class Dog(Tamagochi):
+    def __init__(self, name):
+        super().__init__(name)
+        self.view = f""" 
+                            __
+     ,                    ," e`--o
+    ((                   (  | __,'
+     \.\~----------------' \_;/
+      (                      /
+     /) ._______________.  )
+    (( (               (( (
+     ``-'               ``-'"""
 
-
+    def rais(self, level):
+            self._lv += level
+            if self._lv == 2:
+                self.view = f"""   
+          ____
+         / |   |\.
+  _____/ @ |   | \.
+ |> . .    |   |  \.
+  \  .     |||||     \________________________
+   |||||||\                                    )
+            \                                 |
+             \                                |
+               \                             /
+                |   ____________------\     |
+                |  | |                ||    /
+                |  | |                ||  |
+                |  | |                ||  |
+                |  | |                ||  |  
+               (__/_/                ((__/"""
+            elif self._lv >= 3:
+                self.view = f"""
+       __,-----._                       ,-. 
+     ,'   ,-.    \`---.          ,-----<._/ 
+    (,.-. o:.`    )),"\.\-._    ,'         `. 
+   ('"-` .\       \`:_ )\  `-;'-._          \. 
+  ,,-.    \` ;  :  \( `-'     ) -._     :   `: 
+ (    \ `._\.\ ` ;             ;    `    :    ) 
+  \`.  `-.    __   ,         /  \        ;, ( 
+   `.`-.___--'  `-          /    ;     | :   | 
+     `-' `-.`--._          '           ;     | 
+           (`--._`.                ;   /\    | 
+            \     '                \  ,  )   : 
+            |  `--::----            \.   ;  ;| 
+            \    .__,-      (        )   :  :| 
+             \    : `------; \      |    |   ; 
+              \   :       / , )     |    |  ( 
+               \   \      `-^-|     |   / , ,\ .
+                )  )          | -^- ;   `-^-^' 
+             _,' _ ;          |    | 
+            / , , ,'         /---. : 
+            `-^-^'          (  :  :,' 
+                             `-^--' """
 
 if __name__ == "__main__":
+    pet_type = input("Выберите питомца (1 - Осьминог, 2 - Собака): ")
     name = input("Введите имя: ")
-    octo = Octopus(name)
-    print(octo)
+
+    if pet_type == "1":
+        pet = Octopus(name)
+    elif pet_type == "2":
+        pet = Dog(name)
+    else:
+        print("Неверный выбор. Создан осьминог по умолчанию.")
+        pet = Octopus(name)
+    print(pet)
     action = input("Выберите действие:\nq - выход, b - бить, h - лечить, f - кормить, x - повысить уровень\n")
-    while action:
+    while True:
         if action == 'b':
             hit = int(input("Введите урон: "))
-            octo.bit(hit)
-            print(octo)
+            pet.bit(hit)
+            print(pet)
         elif action == "h":
             hp = int(input("Введите здоровье: "))
-            octo.heal(hp)
-            print(octo)
+            pet.heal(hp)
+            print(pet)
         elif action == "f":
             food = int(input("Введите еду: "))
-            octo.feed(food)
-            print(octo)
+            pet.feed(food)
+            print(pet)
         elif action == "x":
             level = +1
-            octo.rais(level)
-            print(octo)
+            pet.rais(level)
+            print(pet)
         elif action == "q":
             print("Спасибо за игру!")
             break
         else:
             print("Неверный ввод")
-        if not octo.is_alive:
-            print(f"\n{octo.name} умер... Игра окончена.")
+        if not pet.is_alive:
+            print(f"\n{pet.name} умер... Игра окончена.")
             break
         action = input("Выберите действие:\nq - выход, b - бить, h - лечить, f - кормить, x - повысить уровень\n")
